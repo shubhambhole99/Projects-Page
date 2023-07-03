82383e3a02c7cac7a79544a9c69bbd32301c3d9c
sbhole61299 <shubhambhole68@gmail.com>
2023-07-03 23:05:21 +0530

    f

---
+++ my-react-app/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+const app = express();
+
+app.use(express.static('dist'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
+
+const port = process.env.port || 4000; // Adjust the port as needed
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
